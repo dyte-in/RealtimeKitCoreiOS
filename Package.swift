83ae7107f4977b8d11348b520ba535ff22fb7e62
dyte-devel <76557270+dyte-devel@users.noreply.github.com>
2025-06-20 07:23:45 +0000

    chore(release): update SPM definition

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://dyte-assets.s3.ap-south-1.amazonaws.com/sdk/ios_core/RealtimeKit-1.0.0-63157abe-26e4-4872-89ec-77afe153cfe8.xcframework.zip"
-let remoteKotlinChecksum = "9abc050fe7258789f1bb4758f009828d743306ccaa4eea10d14047ace5cd5606"
+let remoteKotlinUrl = "https://sdk-assets.realtime.cloudflare.com/RealtimeKit-1.0.1-58810ff5-f86f-425b-8980-1435d4e90932.xcframework.zip"
+let remoteKotlinChecksum = "a3b2d236f8faad81a40bbb2062070cffe7a01fccb995567cc2c55d636fba8b27"
 let packageName = "RealtimeKit"
 // END KMMBRIDGE BLOCK
 
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DyteWebRTC",
-            url: "https://dyte-assets.s3.ap-south-1.amazonaws.com/sdk/ios_core/DyteWebRTC_v0.0.4.zip",
+            url: "https://sdk-assets.realtime.cloudflare.com/DyteWebRTC_v0.0.4.zip",
             checksum: "25318dfb4bd018fde6ed7fd3337d9aa1c62fc8b39ab985c60fa530eb3819e68a"
         ),
         .binaryTarget(
